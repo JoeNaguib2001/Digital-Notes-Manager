ea08bcb82c9934c2cc7b94e94fd4f69cdb44239a
mario emad <mario emad@LAPTOP-7A47C1CF>
2025-05-28 02:28:59 +0300

    Create Login

---
+++ Digital Notes Manager/Digital Notes Manager/Login.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Digital_Notes_Manager
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            userNameLabel = new DevExpress.XtraEditors.LabelControl();
+            passwordLabel = new DevExpress.XtraEditors.LabelControl();
+            loginBtn = new DevExpress.XtraEditors.SimpleButton();
+            userNameTxt = new DevExpress.XtraEditors.TextEdit();
+            passwordTxt = new DevExpress.XtraEditors.TextEdit();
+            ((System.ComponentModel.ISupportInitialize)userNameTxt.Properties).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)passwordTxt.Properties).BeginInit();
+            SuspendLayout();
+            // 
+            // userNameLabel
+            // 
+            userNameLabel.Appearance.Font = new Font("Arial", 24F, FontStyle.Bold, GraphicsUnit.Point, 178);
+            userNameLabel.Appearance.ForeColor = Color.FromArgb(150, 150, 170);
+            userNameLabel.Appearance.Options.UseFont = true;
+            userNameLabel.Appearance.Options.UseForeColor = true;
+            userNameLabel.Location = new Point(120, 70);
+            userNameLabel.Name = "userNameLabel";
+            userNameLabel.Size = new Size(206, 46);
+            userNameLabel.TabIndex = 0;
+            userNameLabel.Text = "Username:";
+            // 
+            // passwordLabel
+            // 
+            passwordLabel.Appearance.Font = new Font("Arial", 24F, FontStyle.Bold, GraphicsUnit.Point, 178);
+            passwordLabel.Appearance.ForeColor = Color.FromArgb(150, 150, 170);
+            passwordLabel.Appearance.Options.UseFont = true;
+            passwordLabel.Appearance.Options.UseForeColor = true;
+            passwordLabel.Location = new Point(120, 224);
+            passwordLabel.Name = "passwordLabel";
+            passwordLabel.Size = new Size(202, 46);
+            passwordLabel.TabIndex = 1;
+            passwordLabel.Text = "Password:";
+            // 
+            // loginBtn
+            // 
+            loginBtn.Appearance.BackColor = Color.FromArgb(20, 150, 150);
+            loginBtn.Appearance.Font = new Font("Tahoma", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            loginBtn.Appearance.ForeColor = Color.White;
+            loginBtn.Appearance.Options.UseBackColor = true;
+            loginBtn.Appearance.Options.UseFont = true;
+            loginBtn.Appearance.Options.UseForeColor = true;
+            loginBtn.Location = new Point(322, 329);
+            loginBtn.Name = "loginBtn";
+            loginBtn.Size = new Size(161, 68);
+            loginBtn.TabIndex = 4;
+            loginBtn.Text = "Login";
+            loginBtn.Click += loginBtn_Click;
+            // 
+            // userNameTxt
+            // 
+            userNameTxt.Location = new Point(438, 64);
+            userNameTxt.Name = "userNameTxt";
+            userNameTxt.Properties.Appearance.BackColor = Color.FromArgb(84, 94, 106);
+            userNameTxt.Properties.Appearance.Font = new Font("Tahoma", 22.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            userNameTxt.Properties.Appearance.Options.UseBackColor = true;
+            userNameTxt.Properties.Appearance.Options.UseFont = true;
+            userNameTxt.Size = new Size(241, 52);
+            userNameTxt.TabIndex = 5;
+            // 
+            // passwordTxt
+            // 
+            passwordTxt.Location = new Point(438, 218);
+            passwordTxt.Name = "passwordTxt";
+            passwordTxt.Properties.Appearance.BackColor = Color.FromArgb(84, 94, 106);
+            passwordTxt.Properties.Appearance.Font = new Font("Tahoma", 22.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            passwordTxt.Properties.Appearance.Options.UseBackColor = true;
+            passwordTxt.Properties.Appearance.Options.UseFont = true;
+            passwordTxt.Size = new Size(241, 52);
+            passwordTxt.TabIndex = 6;
+            // 
+            // Login
+            // 
+            Appearance.BackColor = Color.FromArgb(46, 49, 56);
+            Appearance.Options.UseBackColor = true;
+            AutoScaleDimensions = new SizeF(7F, 16F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(passwordTxt);
+            Controls.Add(userNameTxt);
+            Controls.Add(loginBtn);
+            Controls.Add(passwordLabel);
+            Controls.Add(userNameLabel);
+            Name = "Login";
+            Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)userNameTxt.Properties).EndInit();
+            ((System.ComponentModel.ISupportInitialize)passwordTxt.Properties).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.LabelControl userNameLabel;
+        private DevExpress.XtraEditors.LabelControl passwordLabel;
+        private DevExpress.XtraEditors.SimpleButton loginBtn;
+        private DevExpress.XtraEditors.TextEdit userNameTxt;
+        private DevExpress.XtraEditors.TextEdit passwordTxt;
+    }
+}
