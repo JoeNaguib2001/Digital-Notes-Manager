bada75d0b7098c163e9135f29091c8512e972a86
Youssef Mohsen <111014510+JoeNaguib2001@users.noreply.github.com>
2025-05-28 02:13:26 +0300

    2asd

--- Digital Notes Manager/Digital Notes Manager/Program.cs
+++ Digital Notes Manager/Digital Notes Manager/Program.cs
@@ -12,6 +12,7 @@ namespace Digital_Notes_Manager
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+            //Sayed Ya Sayed
         }
     }
 }
