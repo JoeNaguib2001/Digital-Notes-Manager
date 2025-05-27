74fdef9c468f30957e4cc9dc95525ea07dcda5a1
sayedAli <said.ali199859@gmail.com>
2025-05-28 02:32:00 +0300

    xform

--- Digital Notes Manager/Digital Notes Manager/Note.cs
+++ Digital Notes Manager/Digital Notes Manager/Note.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using DevExpress.XtraEditors;
 
 namespace Digital_Notes_Manager
 {
-    public partial class Note : Form
+    public partial class Note : XtraForm
     {
         public Note()
         {
