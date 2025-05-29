c5ef6e063b4bc4e5a104e95498d48bbe548db24d
marioemad18 <marioemad108@gmail.com>
2025-05-29 18:14:12 +0300

    Add settings file to login

--- Digital Notes Manager/Digital Notes Manager/Login.cs
+++ Digital Notes Manager/Digital Notes Manager/Login.cs
@@ -21,15 +21,22 @@ namespace Digital_Notes_Manager
 
                 if (user != null)
                 {
+                    Properties.Settings.Default.UserID = user.UserID;
+                    Properties.Settings.Default.Save();
+
+                    XtraMessageBox.Show("Login successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+                    Main_Form main = new Main_Form();
+                    main.Show();
                     this.Hide();
-                    Form form = new Main_Form();
-                    this.Hide();
-                    form.ShowDialog();
                 }
                 else
                 {
                     XtraMessageBox.Show("Wrong Username or Password");
+                    return;
                 }
+
             }
         }
     }
