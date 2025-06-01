cb6b0863168611e0182dcc6e3161b80c103f7ed1
Youssef Mohsen <111014510+JoeNaguib2001@users.noreply.github.com>
2025-06-01 17:19:48 +0300

    ابو السيد

--- Digital Notes Manager/Digital Notes Manager/Main_Form.cs
+++ Digital Notes Manager/Digital Notes Manager/Main_Form.cs
@@ -79,7 +79,6 @@ namespace Digital_Notes_Manager
 
         private void Logout()
         {
-<<<<<<< Updated upstream
             this.Hide();
             Utilities.LoginRegisterMDI.Show();
             Utilities.LoginRegisterMDI.LoadLogin();
@@ -87,18 +86,7 @@ namespace Digital_Notes_Manager
             Properties.Settings.Default.userName = string.Empty;
             Properties.Settings.Default.rememberMe = false;
             Properties.Settings.Default.Save();
-=======
-            //var list = manageNoteContext.Notes.ToList();
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    Note_Form note_form = new Note_Form();
-            //    note_form.richTextBox1.Text = list[i].Content;
-            //    note_form.Text = list[i].Title;
-            //    note_form.Show();
 
-            //}
-            this.MDI_Panel.Controls.Clear();
->>>>>>> Stashed changes
         }
 
         private void LoadAllNotesPoppedOut()
--- Digital Notes Manager/Digital Notes Manager/Testing.Designer.cs
+++ Digital Notes Manager/Digital Notes Manager/Testing.Designer.cs
@@ -29,19 +29,13 @@
         private void InitializeComponent()
         {
             accordionControl1 = new DevExpress.XtraBars.Navigation.AccordionControl();
-            accordionControlElement1 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
-            accordionControlElement2 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
-            comboBox1 = new ComboBox();
-            dateTimePicker1 = new DateTimePicker();
-            richTextBox1 = new RichTextBox();
-            richTextBox2 = new RichTextBox();
+            flowLayoutPanel1 = new FlowLayoutPanel();
             button1 = new Button();
             ((System.ComponentModel.ISupportInitialize)accordionControl1).BeginInit();
             SuspendLayout();
             // 
             // accordionControl1
             // 
-            accordionControl1.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] { accordionControlElement1, accordionControlElement2 });
             accordionControl1.Location = new Point(22, 50);
             accordionControl1.LookAndFeel.SkinMaskColor = Color.FromArgb(255, 192, 255);
             accordionControl1.LookAndFeel.SkinMaskColor2 = Color.FromArgb(255, 224, 192);
@@ -50,52 +44,21 @@
             accordionControl1.Size = new Size(260, 300);
             accordionControl1.TabIndex = 0;
             // 
-            // accordionControlElement1
+            // flowLayoutPanel1
             // 
-            accordionControlElement1.Appearance.Normal.BackColor = Color.Red;
-            accordionControlElement1.Appearance.Normal.Options.UseBackColor = true;
-            accordionControlElement1.Name = "accordionControlElement1";
-            accordionControlElement1.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
-            accordionControlElement1.Text = "Element1";
-            // 
-            // accordionControlElement2
-            // 
-            accordionControlElement2.Appearance.Normal.BackColor = Color.Red;
-            accordionControlElement2.Appearance.Normal.Options.UseBackColor = true;
-            accordionControlElement2.Name = "accordionControlElement2";
-            accordionControlElement2.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
-            accordionControlElement2.Text = "Element2";
-            // 
-            // comboBox1
-            // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(308, 224);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(121, 23);
-            comboBox1.TabIndex = 1;
-            // 
-            // richTextBox1
-            // 
-            richTextBox1.Location = new Point(350, 237);
-            richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(100, 96);
-            richTextBox1.TabIndex = 2;
-            richTextBox1.Text = "";
-            // 
-            // richTextBox2
-            // 
-            richTextBox2.Location = new Point(513, 237);
-            richTextBox2.Name = "richTextBox2";
-            richTextBox2.Size = new Size(100, 96);
-            richTextBox2.TabIndex = 3;
-            richTextBox2.Text = "";
+            flowLayoutPanel1.Dock = DockStyle.Top;
+            flowLayoutPanel1.Location = new Point(0, 0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(800, 224);
+            flowLayoutPanel1.TabIndex = 1;
             // 
             // button1
             // 
-            button1.Location = new Point(448, 370);
+            button1.Dock = DockStyle.Fill;
+            button1.Location = new Point(0, 224);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 4;
+            button1.Size = new Size(800, 226);
+            button1.TabIndex = 2;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
@@ -105,11 +68,8 @@
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(comboBox1);
             Controls.Add(button1);
-            Controls.Add(richTextBox2);
-            Controls.Add(richTextBox1);
-            Controls.Add(dateTimePicker1);
+            Controls.Add(flowLayoutPanel1);
             Controls.Add(accordionControl1);
             Name = "Testing";
             Text = "Testing";
@@ -120,12 +80,7 @@
         #endregion
 
         private DevExpress.XtraBars.Navigation.AccordionControl accordionControl1;
-        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement1;
-        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement2;
-        private ComboBox comboBox1;
-        private DateTimePicker dateTimePicker1;
-        private RichTextBox richTextBox1;
-        private RichTextBox richTextBox2;
+        private FlowLayoutPanel flowLayoutPanel1;
         private Button button1;
     }
 }
