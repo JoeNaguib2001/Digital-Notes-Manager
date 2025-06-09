b355aa18c04c94aff8dd9674cced0fb29c026d28
Maged Eshak <magedeshak36@gmail.com>
2025-06-09 09:37:58 +0300

    update Program

--- Digital Notes Manager/Digital Notes Manager/Program.cs
+++ Digital Notes Manager/Digital Notes Manager/Program.cs
@@ -13,53 +13,8 @@ namespace Digital_Notes_Manager
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            List<Note> notes = new List<Note>
-            {
-                new Note()
-                {
-                    ID = 20,
-                    Title = "a",
-                    UserID = 1,
-                    Category = Category.Ideas,
-                    Content = "as",
-                     ReminderDate =new DateTime(2025,3,1),
-
-                },
-                new Note()
-                {
-                    ID = 21,
-                    Title = "a",
-                    UserID = 1,
-                    Category = Category.Reminders,
-                    Content = "as",
-                    ReminderDate =new DateTime(2025,4,1),
-
-                },
-                new Note()
-                {
-                    ID = 22,
-                    Title = "a",
-                    UserID = 1,
-                    Category = Category.Study,
-                    Content = "as",
-                    StartDate = DateTime.Now,
-                    ReminderDate =new DateTime(2025,7,1),
-
-                },
-                new Note()
-                {
-                    ID = 23,
-                    Title = "a",
-                    UserID = 1,
-                    Category = Category.Work,
-                    Content = "as",
-                     ReminderDate =new DateTime(2025,6,4),
-
-                }
-            };
-
-
-            Application.Run(new ReportsForm(notes));
+           
+            Application.Run(new LoginRegisterMDI());
         }
     }
 }
